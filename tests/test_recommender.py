a03a8b6ed019c4a6683a5487b941232a7d389725
Yulia Perunovskaia <yulia.perunovskaia@mutiny.group>
2024-02-18 14:34:41 +1100

    removed loader test

--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -1,20 +1,15 @@
+import pytest
 import numpy as np
 import pandas as pd
 from recommender.recommender import Word2VecTagRecommender
 
 
-sentences = [['apple', 'banana', 'orange'], ['apple', 'orange'], ['banana', 'kiwi']]
+sentences = [["apple", "banana", "orange"], ["apple", "orange"], ["banana", "kiwi"]]
 
 
 def test_train_model():
     recommender = Word2VecTagRecommender()
     recommender.train_model(sentences)
-    assert recommender.model is not None
-
-
-def test_load_model():
-    recommender = Word2VecTagRecommender()
-    recommender.load_model(path="tests/test_word2vec.model")
     assert recommender.model is not None
 
 
@@ -28,7 +23,7 @@ def test_get_embedding():
 def test_recommend_tags():
     recommender = Word2VecTagRecommender()
     recommender.train_model(sentences)
-    tags = pd.Series([['apple', 'banana'], ['banana', 'kiwi']])
+    tags = pd.Series([["apple", "banana"], ["banana", "kiwi"]])
     recommended_tags = recommender.recommend_tags("apple", tags)
     print(tags)
     assert len(recommended_tags) > 0
