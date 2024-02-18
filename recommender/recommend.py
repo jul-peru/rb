6b5400a92be00108bbe9135780f9532539fb8ec5
Yulia Perunovskaia <yulia.perunovskaia@mutiny.group>
2024-02-18 17:38:20 +1100

    config is moved outside function

--- recommender/recommend.py
+++ recommender/recommend.py
@@ -1,3 +1,6 @@
+"""
+Recommendation pipeline.
+"""
 from dataloader import CSVDataLoader
 from loguru import logger
 from preprocessing import get_uniques, preprocess_input
@@ -11,29 +14,30 @@ parser.add_argument("--num_results", type=int, default=3,
                     help="Number of recommended tags (default: 3). Set value between 1 and 15")
 parser.add_argument("search_query", type=str, help="Search query")
 
+# TODO Move config to a separate file
+config = {
+    "input_tags_path": "artist_tags.csv",
+    "tags_column": "tags",
+    "output_file": "results.json",
+    "model_path": "word2vec_artwork_search.model",
+}
 
-def main():
+
+def main(config):
     args = parser.parse_args()
     num_results = args.num_results
     search_query = args.search_query
     if (num_results < 1) | (num_results > 15):
         raise ValueError("Number of recommended tags must be between 1 and 15")
-    config = {
-        "input_tags_path": "artist_tags.csv",
-        "tags_column": "tags",
-        "output_file": "results.json"
-    }
 
     csv_loader = CSVDataLoader()
     tags_column = config["tags_column"]
     artists_tags = csv_loader.load_data(
-        config["input_tags_path"], header=None, names=["tags"]
+        config["input_tags_path"], header=None, names=[config["tags_column"]]
         )
     tags_processed = preprocess_input(artists_tags, tags_column, token="comb")
-
     recommender = Word2VecTagRecommender()
-
-    model_path = "word2vec_artwork_search.model"
+    model_path = config["model_path"]
     recommender.load_model(model_path)
 
     recommended_tags = recommender.recommend_tags(
@@ -47,4 +51,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(config)
