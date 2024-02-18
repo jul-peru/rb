9bb0ecf466c0d5713e218d0a7719588382f5c227
Yulia Perunovskaia <yulia.perunovskaia@mutiny.group>
2024-02-18 17:36:42 +1100

    class description added, returns added

--- recommender/dataloader.py
+++ recommender/dataloader.py
@@ -1,10 +1,24 @@
 import os
 import pandas as pd
+from loguru import logger
 
 
 # Add as a class so we can initiate any needed connections in the future to load data
 # otherwise can leave it to be a function.
 class CSVDataLoader:
+    """
+    A class for loading CSV or TSV format files into pandas DataFrame.
+
+    Attributes
+    ----------
+    Further attributes could be introduced if any additional connection
+    would be required.
+
+    Methods
+    -------
+    load_data(filepath, header="infer", names=None):
+        Loads csv/tsv format files from specified path.
+    """
     def __init__(self):
         self.df = None
 
@@ -20,6 +34,11 @@ class CSVDataLoader:
             row (0-indexed) to use as header or None if there is no header
         names : list
             list of column names to use
+        
+        Returns
+        -------
+        df : pd.DataFrame
+            Pandas dataframe from loaded file
         """
         _, file_extension = os.path.splitext(filepath)
         file_extension = file_extension.lower()
@@ -33,6 +52,6 @@ class CSVDataLoader:
                 f"Unsupported file extension: {file_extension}. Use 'csv' or 'tsv'."
                 )
         self.df = pd.read_csv(filepath, header=header, sep=sep, names=names)
-        print(f"{filepath} file loaded successfully.")
+        logger.info(f"{filepath} file loaded successfully.")
 
         return self.df
